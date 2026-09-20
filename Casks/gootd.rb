49ae05ad9a54e2457d686eab3a273d1a9b8e12be
dexrfy0000 <dexrfy0000@users.noreply.github.com>
2026-09-21 00:39:37 +0300

    Add Gootd cask

---
+++ Casks/gootd.rb
@@ -0,0 +1,11 @@
+cask "gootd" do
+  version "1.0"
+  sha256 "6b06e8991ada45052b9ecfd87390517f500665055b0f5e94e9695a9c0e690c13"
+
+  url "https://gootd.vercel.app/Gootd.dmg"
+  name "Gootd"
+  desc "Turn off screen and keyboard light with one hotkey"
+  homepage "https://gootd.vercel.app/"
+
+  app "Gootd.app"
+end
